bc5616f042bd550c9945192637782bd8cc365579
Mit <mit.pandya@thegatewaycorp.co.in>
2024-09-03 15:19:35 +0530

    VAi deploy hojana ess bar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -100,6 +100,7 @@ const Card = ({ data }: { data: PizzaData }) => {
           </p>
           <button
             onClick={() => {
+              //@ts-ignore
               addToCart(data);
             }}
             className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
